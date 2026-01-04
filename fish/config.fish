ccd28ff79339e3d46dfaff8155b99865918e2c32
InKryption <inkryption07@gmail.com>
2026-01-04 20:52:38 +0100

    fish: warn about fish version < 4.3.0

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,11 @@ if status is-interactive
 
     abbr cls clear
     abbr 'cd..' 'cd ..'
+
+    set -l version_as_num (string replace '.' '' $version --all)
+    if test $version_as_num -lt 430
+        echo "Warning: fish version $version is < 4.3.0"
+    end
 end
 
 if test -f ~/.fishrc
